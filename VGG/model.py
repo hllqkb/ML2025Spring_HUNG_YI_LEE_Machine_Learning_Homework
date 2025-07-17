547eb19bb8c10fe416a9cd84e839216f88232829
hllqkb <androidhtml@yandex.com>
2025-07-18 00:53:21 +0800

    feat(VGG): 添加VGG模型实现及其训练代码
    在`model.py`中添加了VGG模型的实现，包括特征提取层和分类器层，并实现了权重初始化功能。
    
    在`train.py`中添加了使用VGG模型进行训练的代码，包括数据增强、数据加载、模型定义、损失函数和优化器的选择，以及训练和验证过程的实现。代码中定义了训练和验证数据集的加载方式，并通过循环实现了20个epoch的训练过程，每轮训练后进行验证，并保存最佳验证准确率的模型权重。

---
+++ VGG/model.py
@@ -0,0 +1,61 @@
+from torch import nn
+import torch.nn.functional as F
+import torchvision.models as models
+import torch
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=1000, init_weights=False):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+           nn.Dropout(0.5),
+           nn.Linear(512 * 7 * 7, 4096),
+           nn.ReLU(True),
+           nn.Dropout(0.5), # 在全连接层2之前加一个Dropout层
+           nn.Linear(4096, 4096),
+           nn.ReLU(True), # 再加一个ReLU层，j激活函数
+           nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1) # 展平from [batch_size, 512, 7, 7] to [batch_size, 512*7*7]
+        x = self.classifier(x)
+        return x
+# xavier是针对sig和tanh函数，防止梯度消失，凯明初始化，针对ru
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.constant_(m.bias, 0)
+
+def make_features(cfg: list):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfgs = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+def vgg(model_name='vgg16', **kwargs):
+    cfg = cfgs[model_name]
+    model = VGG(make_features(cfg), **kwargs)
+    return model
+# load model
+# vgg()
---
+++ VGG/train.py
@@ -0,0 +1,100 @@
+from torchvision import datasets, transforms, utils
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import os, json, time
+import numpy as np
+import torchvision
+from model import VG,vgg
+import matplotlib.pyplot as plt
+from PIL import Image
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+# 进行数据增强
+datatransform = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),  # 随机裁剪到224*224
+        transforms.RandomHorizontalFlip(),  # 随机水平翻转
+        transforms.ToTensor(),  # 转为tensor
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # 归一化
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),  # 先将图片resize到256*256
+        transforms.CenterCrop(224),  # 然后中心裁剪到224*224
+        transforms.ToTensor(),  # 转为tensor
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # 归一化
+    ])
+}
+
+trainset = datasets.ImageFolder(root='./flower_data/train', transform=datatransform['train'])
+print(len(trainset))
+flower_list = trainset.class_to_idx
+cla_dict = dict((val, key) for key, val in flower_list.items())
+json_str = json.dumps(cla_dict, indent=4)
+with open('flower_dict.json', 'w') as json_file:
+    json_file.write(json_str)
+
+batch_size = 32
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+testset = datasets.ImageFolder(root='./flower_data/val', transform=datatransform['val'])
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True, num_workers=4)
+
+# testdata_iter = iter(testloader)
+# images, labels = next(testdata_iter)
+
+# def showimg(img):
+#     img = img / 2 + 0.5
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#     plt.show()
+# print(' '.join('%5s' % cla_dict[labels[j].item()] for j in range(4)))
+# showimg(utils.make_grid(images))
+
+# 定义AlexNet模型
+model =vgg(model_name='vgg16', num_classes=5, init_weights=True)
+model.to(device)
+criterion = nn.CrossEntropyLoss()
+# optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.Adam(model.parameters(), lr=0.0002)
+save_path='./vgg16_model.pth'
+best_acc = 0.0
+for epoch in range(40):
+    model.train()
+    t1=time.perf_counter()
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        # print train process
+        rate=(i+1)/len(trainloader)
+        a="*" * int(rate*50)
+        b="." * (50-int(rate*50))
+        print("\rtrain epoch[{}/{}] loss:{:.3f} [{}{}]".format(epoch+1,20,running_loss/(i+1),a,b),end="")
+    print()
+    print("train epoch[{}/{}] loss:{:.3f} time:{:.2f}s".format(epoch+1,20,running_loss/(i+1),time.perf_counter()-t1))
+    model.eval()
+    acc=0.0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            acc += (predicted == labels).sum().item()
+    acc /= len(testset)
+    if acc > best_acc:
+        best_acc = acc
+        torch.save(model.state_dict(), save_path)
+    print('test accuracy: {:.2f} %'.format(acc*100))
+
+print('Finished Training')
+# the best test accuracy is about 80 %
