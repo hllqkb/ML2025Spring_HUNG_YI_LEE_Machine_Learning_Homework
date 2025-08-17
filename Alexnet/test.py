4333039e2801d63a7d0c1bed28f73ea45f33e03f
hllqkb <androidhtml@yandex.com>
2025-08-17 22:47:27 +0800

    feat: 添加目录操作测试示例
    在test.py文件中添加了使用shutil模块进行目录操作的测试示例，包括快速删除目录、复制目录、移动目录和生成压缩文件的功能。
    
    - 添加了快速删除目录及其所有内容的示例代码：`shutil.rmtree('tts')`
    - 添加了复制目录及其所有内容的示例代码：`shutil.copytree('tts', 'tts_copy')`
    - 添加了移动目录及其所有内容的示例代码：`shutil.move('tts_copy', 'tts_move')`
    - 添加了生成压缩文件的示例代码：`shutil.make_archive("test_archive", "zip", "test_dir/")`

---
+++ Alexnet/test.py
@@ -0,0 +1,9 @@
+import shutil
+# 快速删除一个目录及其所有内容
+# shutil.rmtree('tts') 
+# 复制一个目录及其所有内容
+# shutil.copytree('tts', 'tts_copy') 
+# 移动一个目录及其所有内容
+# shutil.move('tts_copy', 'tts_move')
+# 产生压缩文件
+# shutil.make_archive("test_archive", "zip", "test_dir/")
