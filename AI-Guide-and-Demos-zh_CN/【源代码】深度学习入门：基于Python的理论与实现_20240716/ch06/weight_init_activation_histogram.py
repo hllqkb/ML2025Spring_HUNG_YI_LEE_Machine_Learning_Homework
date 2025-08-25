024746f97350467350817ab612c94e5475389651
hllqkb <androidhtml@yandex.com>
2025-08-25 16:03:52 +0800

    动手学深度学习入门

--- AI-Guide-and-Demos-zh_CN/【源代码】深度学习入门：基于Python的理论与实现_20240716/ch06/weight_init_activation_histogram.py
+++ AI-Guide-and-Demos-zh_CN/【源代码】深度学习入门：基于Python的理论与实现_20240716/ch06/weight_init_activation_histogram.py
@@ -26,8 +26,8 @@ for i in range(hidden_layer_size):
         x = activations[i-1]
 
     # 改变初始值进行实验！
-    w = np.random.randn(node_num, node_num) * 1
-    # w = np.random.randn(node_num, node_num) * 0.01
+    # w = np.random.randn(node_num, node_num) * 1
+    w = np.random.randn(node_num, node_num) * 0.01
     # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num)
     # w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num)
 
@@ -36,8 +36,8 @@ for i in range(hidden_layer_size):
 
 
     # 将激活函数的种类也改变，来进行实验！
-    z = sigmoid(a)
-    # z = ReLU(a)
+    # z = sigmoid(a)
+    z = ReLU(a)
     # z = tanh(a)
 
     activations[i] = z
